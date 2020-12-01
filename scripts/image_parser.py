fdfbcba319dc4048e21c6ad8ebc57a034f8db738
Angelo Rosace <angelo96rosace@gmail.com>
2020-12-01 21:32:02 +0100

    added flag and info to tasks

--- scripts/image_parser.py
+++ scripts/image_parser.py
@@ -6,16 +6,32 @@ from tensorflow.keras import layers
 import cv2 as cv
 import os
 from glob import glob
+import sys
+import argparse
+
+my_parser = argparse.ArgumentParser(description='image parser')
+my_parser.add_argument('--user',
+                       metavar='user',
+                       type=str,
+                       help='Who is using the script')
+args = my_parser.parse_args()
+
+user = args.user
 
 # Prepare file directories and image paths
-os.chdir('/Users/zacharycollester/Documents/')
-PATH = 'cv_chest/data/sample/'
-SOURCE_IMAGES = os.path.join(PATH, "sample", "images")
-images = glob(os.path.join(SOURCE_IMAGES, "*.png"))
+if user == "zach":
+	os.chdir('/Users/zacharycollester/Documents/')
+	PATH = 'cv_chest/data/sample/'
+	SOURCE_IMAGES = os.path.join(PATH, "sample", "images")
+	images = glob(os.path.join(SOURCE_IMAGES, "*.png"))
 
-# Load labels
-labels = pd.read_csv(os.path.join(PATH, 'sample_labels.csv'))
-
+	# Load labels
+	labels = pd.read_csv(os.path.join(PATH, 'sample_labels.csv'))
+elif user == "angelo":
+	os.chdir('/home/angelo/Desktop/cv_chest/')
+	PATH = 'data/sample/'
+	SOURCE_IMAGES = os.path.join(PATH, 'sample', 'images')
+	images = glob(os.path.join(SOURCE_IMAGES, "*.png"))
 
 def process_images():
     """
@@ -33,7 +49,3 @@ def process_images():
     return x
 
 x = process_images()
-
-
-cv.waitKey(0)
-cv.destroyAllWindows()
